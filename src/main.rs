23f9cc5e22557da5843b6ad453fb7774b9684eb1
zX3no <dr_xeno@hotmail.com.au>
2021-09-20 07:40:09 +0930

    removed unwraps for mpd

--- src/main.rs
+++ src/main.rs
@@ -6,41 +6,49 @@ use mpd::Client;
 static IP: &str = "192.168.0.100:6600";
 
 fn pause() {
-    let mut conn = Client::connect(IP).unwrap();
-    conn.toggle_pause().unwrap();
+    if let Ok(mut conn) = Client::connect(IP) {
+        conn.toggle_pause().ok();
+    }
 }
 
 fn prev() {
-    let mut conn = Client::connect(IP).unwrap();
-    conn.prev().unwrap();
+    if let Ok(mut conn) = Client::connect(IP) {
+        conn.prev().ok();
+    }
 }
 
 fn next() {
-    let mut conn = Client::connect(IP).unwrap();
-    conn.next().unwrap();
+    if let Ok(mut conn) = Client::connect(IP) {
+        conn.next().unwrap_or(());
+    }
 }
 
 fn vol_up() {
-    let mut conn = Client::connect(IP).unwrap();
-    let vol = conn.status().unwrap().volume;
-
-    if vol + 5 <= 100 {
-        conn.volume(vol + 5).unwrap();
-    } else {
-        conn.volume(100).unwrap();
+    if let Ok(mut conn) = Client::connect(IP) {
+        if let Ok(status) = conn.status() {
+            let vol = status.volume;
+            if vol + 5 <= 100 {
+                conn.volume(vol + 5).unwrap();
+            } else {
+                conn.volume(100).unwrap();
+            }
+        }
     }
 }
 
 fn vol_down() {
-    let mut conn = Client::connect(IP).unwrap();
-    let vol = conn.status().unwrap().volume;
-
-    if vol - 5 >= 0 {
-        conn.volume(vol - 5).unwrap();
-    } else {
-        conn.volume(0).unwrap();
+    if let Ok(mut conn) = Client::connect(IP) {
+        if let Ok(status) = conn.status() {
+            let vol = status.volume;
+            if vol - 5 <= 100 {
+                conn.volume(vol - 5).unwrap();
+            } else {
+                conn.volume(0).unwrap();
+            }
+        }
     }
 }
+
 fn main() {
     let mut hk = Listener::new();
     let alt_shift = hotkey::modifiers::ALT | hotkey::modifiers::SHIFT;
@@ -48,7 +56,7 @@ fn main() {
     hk.register_hotkey(alt_shift, 'W' as u32, next).unwrap();
     hk.register_hotkey(alt_shift, '1' as u32, vol_down).unwrap();
     hk.register_hotkey(alt_shift, '2' as u32, vol_up).unwrap();
-    hk.register_hotkey(modifiers::SHIFT, keys::BACKSPACE, pause)
+    hk.register_hotkey(modifiers::SHIFT, keys::ESCAPE, pause)
         .unwrap();
     //115 is f4
     hk.register_hotkey(alt_shift | modifiers::CONTROL, 115_u32, || {
